eae9e140b8ef7b2fdce70f323544cfa5e569053a
Timur <timbyxtyt@gmail.com>
2024-04-26 02:38:25 +0000

    backtests

---
+++ src/exchange/bybit/bybit_backtest.py
@@ -0,0 +1,119 @@
+import os
+import time
+from datetime import timedelta
+
+import pandas as pd
+
+from src import allowed_range, delta, logger, sync_obj_with_config
+from src.exchange.backtest import BackTest
+from src.exchange.bybit.bybit_stub import BybitStub
+from src.exchange_config import exchange_config
+
+
+class BybitBackTest(BackTest, BybitStub):
+    update_data = True
+    minute_granularity = False
+    check_candles_flag = True
+    days = 120
+    search_oldest = 10
+    enable_trade_log = True
+    start_balance = 0
+    warmup_tf = None
+
+    def __init__(self, account, pair):
+        """
+        Constructor for BinanceFuturesBackTest class.
+        Args:
+            account (str): The account to use for the backtest.
+            pair (str): The trading pair to backtest.
+        """
+        BybitStub.__init__(self, account, pair=pair, threading=False)
+        BackTest.__init__(self)
+
+        self.pair = pair
+
+        sync_obj_with_config(exchange_config["bybit"], BybitBackTest, self)
+
+    def on_update(self, bin_size, strategy):
+        """
+        Register the strategy function.
+        Args:
+            bin_size (str): The bin size for the OHLCV data.
+            strategy (function): The strategy function to be executed during the backtest.
+        """
+        self.bin_size = bin_size
+        self.set_paths("bybit", pair=self.pair, bin_size=self.bin_size)
+        self.load_ohlcv(bin_size)
+
+        BybitStub.on_update(self, bin_size, strategy)
+        BackTest.crawler_run(self)
+
+    def download_data(self, bin_size, start_time, end_time):
+        """
+        download or get amd return ohlcv data
+        """
+        data = pd.DataFrame()
+        left_time = None
+        source = None
+        is_last_fetch = False
+        file = self.OHLC_FILENAME
+        search_left = self.search_oldest
+        last_search_ts = None
+
+        if self.minute_granularity:
+            bin_size = "1m"
+        else:
+            bin_size = bin_size[0]
+
+        while True:
+            try:
+                if left_time is None:
+                    left_time = start_time
+                    right_time = left_time + delta(allowed_range[bin_size][0]) * 99
+                else:
+                    left_time = source.iloc[-1].name
+                    right_time = left_time + delta(allowed_range[bin_size][0]) * 99
+
+                if right_time > end_time:
+                    right_time = end_time
+                    is_last_fetch = True
+
+            except IndexError:
+                start_time = start_time + timedelta(
+                    days=self.search_oldest if self.search_oldest else 1
+                )
+                left_time = None
+                logger.info(
+                    f"Failed to fetch data, start stime is too far in history.\n\
+                    >>>  Searching, please wait. <<<\n\
+                    Searching for oldest viable historical data, next start time attempt: {start_time}"
+                )
+                time.sleep(0.25)
+                continue
+
+            source = self.fetch_ohlcv(
+                bin_size=bin_size, start_time=left_time, end_time=right_time
+            )
+
+            if search_left and not os.path.exists(file):
+                logger.info(
+                    "Searching for older historical data.\n>>>  Searching, please wait. <<<"
+                )
+                start_time = start_time - timedelta(days=self.search_oldest)
+                left_time = None
+                if len(source) == 0 or (
+                    last_search_ts is not None
+                    and last_search_ts == source.iloc[-1].name
+                ):
+                    search_left = False
+                    continue
+                last_search_ts = source.iloc[-1].name
+                time.sleep(0.25)
+                continue
+
+            data = pd.concat([data, source])
+
+            if is_last_fetch:
+                return data
+
+            time.sleep(0.25)
