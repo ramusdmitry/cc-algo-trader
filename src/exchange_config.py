be127d8ce5ec8331079139490a672a44c9dc7153
Timur <timbyxtyt@gmail.com>
2024-05-03 13:39:43 +0000

    bybit settings in json

---
+++ src/exchange_config.py
@@ -0,0 +1,18 @@
+exchange_config = {
+    "bybit": {
+        "qty_in_usdt": False,
+        "minute_granularity": False,
+        "timeframes_sorted": True,
+        "enable_trade_log": True,
+        "order_update_log": True,
+        "ohlcv_len": 100,
+        "call_strat_on_start": False,
+        "balance": 1000,
+        "leverage": 1,
+        "update_data": True,
+        "check_candles_flag": True,
+        "days": 1200,
+        "search_oldest": 10,
+        "warmup_tf": None,
+    },
+}
